d9c3a4199ecc2858dd75a23a16c711c4c1fd1f82
hilmanzu <hilmanzutech@gmail.com>
2025-07-24 11:50:02 +0700

    feat:

--- obj/Debug/net8.0/beresbackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/beresbackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("beresbackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b70cb4cacd086b53e31e2473aa437e0306f09d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc77f2a388398a6e16890d4a3a8179532b510ea4")]
 [assembly: System.Reflection.AssemblyProductAttribute("beresbackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("beresbackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
